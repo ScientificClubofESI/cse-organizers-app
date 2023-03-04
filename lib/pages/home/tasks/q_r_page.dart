19b96b2cef8ac63854dd1bf009ab8e9b57f8e6ed
Mohammed Tirichine <mohtiri718@gmail.com>
2023-03-04 12:28:51 +0100

    Changes in the hierarchy

---
+++ lib/pages/home/tasks/q_r_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QRPage extends StatelessWidget {
+  const QRPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

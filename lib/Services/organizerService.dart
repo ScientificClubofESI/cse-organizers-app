a9206240c0bee76ee798712000749d3be6bd1a14
Fadoua sla <fadouaslama@gmail.com>
2023-03-11 21:20:50 +0100

    fix the uid err

--- lib/Services/organizerService.dart
+++ lib/Services/organizerService.dart
@@ -1,6 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'classes.dart';
+
 
 
 class DatabaseService {
@@ -19,7 +19,7 @@ class DatabaseService {
       DocumentReference taskDoc = FirebaseFirestore.instance.collection('Events').doc(event).collection(day).doc(taskId);
 
       await taskDoc.update({
-        'checked': FieldValue.arrayUnion(uid)
+        'checked': FieldValue.arrayUnion([uid])
       });
 
     } catch (e) {
